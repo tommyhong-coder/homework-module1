87ad0473c7744044b76a755833e4f8ca8ab874ba
tommy <tommy.hong@ceridian.com>
2022-01-29 12:56:55 -0600

    week4 day2b

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,11 +1,11 @@
 /*----RANDOM CODES -----*/
 //Function to generate combination of characters
-function generateCode() {
     //create variables to store generated codes and teh type of characters we want to show as codes
     var code = ' ';//initialize to null value
     var getCode = ' '; //to store entered code
     var btnvalue; //for button boolean value
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+function generateCode() {
     //Generate character multiple times using a loop
     for (i = 1; i <=8; i++) {
         var char = Math.random()* str.length; //random select a character from the variable and then store in a new variable
@@ -21,11 +21,11 @@ function disableButton(btnvalue) {
     document.getElementById("submit").disabled = btnvalue; //able/disable button
     if (btnvalue == true) { //test if button is disabled or enabled
         //set button and label color translucent
-        document.getElementById("submit").style.backgroundColor = "rgba (73, 119, 209, 0.3)";
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
         document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5";
     } else {
         //setbutton and label color with no transparency
-        document.getElementById("submit").style.backgroundColor = "rgba (73, 119,209, 1)";
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
         document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
     }
 }
@@ -43,4 +43,3 @@ function evaluateCode() {
     }
 }
 //Activate function
-disableButton();
--- js/site_scripts.js
+++ js/site_scripts.js
@@ -3,12 +3,12 @@
                   var fileNames = []; //Declare an empty element to store image file names
                   var imageList = []; //Declare an empty array to store html list that contain an image
                   var image; //Declare an empty variable to store the assemnbled image list codes
-                  var openList = "<li id='partner'>"; //Delare a variable to contain open list tag
+                  var openList = "<li class='partner'>"; //Delare a variable to contain open list tag
                   var closeList = "</li>";  //Delacre a variable to contain close list tag
                   //create a loop to create 6 images starting with index of 0
                   for (var i=0; i<6; i++) {
                       fileNames.push("partner"+(i+1));  //Create image file name and store in array
-                      photos.push("<img src='images/"+fileNames[i]+".png' alt='Partner adventures "+(i+1)+"' width='100'>"); //Assemble file name into image element and store in an array
+                      photos.push("<img src='images/"+fileNames[i]+".png' alt='Partner adventures "+(i+1)+"'width='100'>"); //Assemble file name into image element and store in an array
                       image = openList + photos[i] + closeList;  //Assemble image element from array with list elements and store in a variable
                       imageList.push(image);  //Store(push) the assembled list codes into array
                   }
